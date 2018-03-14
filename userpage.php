8588a3407b4ba454f565725a652445b46e9af0ba
jjohanj <johanjorritsma@hotmail.com>
2018-03-14 10:42:16 +0100

    fixes to frontpage, start userpage

---
+++ userpage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <title>ELO kids</title>
+    <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+
+  </head>
+  <body>
+    <div class="container-fluid">
+    <div class="row">
+      <div class= "col-xs-12">
+        <div class= "col-xs-8">
+        <div class="title">Spelgoed</div></div>
+        <div class= "col-xs-4"><button class='login'>Login</button><button class='login'>FAQ</button>
+        <button class='login'>Info</button>
+        </div>
+      </div>
+
+      <div class= "col-xs-6"></div>
+
+      <div class= "col-xs-6"></div>
+
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+      <div class="col-xs-4"></div>
+
+     </div></div>
+    </div>
+
+  </body>
+
+</html>
