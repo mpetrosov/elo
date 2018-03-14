4aa3110a4cfc5dd90cd83bd5134952349d39bf62
jjohanj <johanjorritsma@hotmail.com>
2018-03-14 12:22:03 +0100

    first userpage

--- userpage.php
+++ userpage.php
@@ -13,26 +13,61 @@
 
   </head>
   <body>
-    <div class="container-fluid">
+    <div class="container">
     <div class="row">
       <div class= "col-xs-12">
-        <div class= "col-xs-8">
-        <div class="title">Spelgoed</div></div>
-        <div class= "col-xs-4"><button class='login'>Login</button><button class='login'>FAQ</button>
-        <button class='login'>Info</button>
+        <div class="row">
+          <div class= "col-xs-8">
+          <div class="title">Spelgoed</div>
+          </div>
+          <div class= "col-xs-4">
+          <button class='login'>Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-8 usertop">
+        <div class="row">
+        <div class="col-xs-8 welcome">Hallo Alia</div>
+        <div class="col-xs-4 avatar"><img src="img/owl.png"></div>
         </div>
       </div>
 
-      <div class= "col-xs-6"></div>
-
-      <div class= "col-xs-6"></div>
-
-      <div class="col-xs-4"></div>
-      <div class="col-xs-4"></div>
-      <div class="col-xs-4"></div>
-
-     </div></div>
+      <div class="col-sm-4 usertop">
+        <div class="profile"> <div class="head">Profiel:</div>naam: Alia Jorritsma<br>leeftijd: 8 jaar<br>
+          groep: 5<br><hr>
+        </div>
+        <div class="points">1000 punten</div>
+      </div>
     </div>
+
+    <div class="row">
+      <div class="col-sm-4 theme"><div class="themetext">Thema 1</div>
+      </div>
+      <div class="col-sm-4 theme"><div class="themetext">Thema 2</div>
+      </div>
+      <div class="col-sm-4 theme"><div class="themetext">Thema 3</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-4 theme"><div class="themetext">Thema 4</div>
+      </div>
+      <div class="col-sm-4 theme"><div class="themetext">Thema 5</div>
+      </div>
+      <div class="col-sm-4 theme"><div class="themetext">Thema 6</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12">
+      </div>
+
+    </div>
+
+  </div>
 
   </body>
 
