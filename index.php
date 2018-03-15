64f7ebf93124460f32850f706f96105ce8b8e7c7
jjohanj <johanjorritsma@hotmail.com>
2018-03-15 11:35:25 +0100

    login and frontpage update

--- index.php
+++ index.php
@@ -1,24 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
 
-include_once 'includes/header.php'
+    <meta charset="utf-8">
+    <title>ELO kids</title>
+    <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 
-?>
-    <section class="main-container">
-        <div class="main-wrapper">
-            <h2>Home</h2>
-            <?php
-                if (isset($_SESSION['u_id'])){
 
-                    echo "You are loged in";
-                }
-            
-            ?>
-           
-        </div>
-    </section>
+  </head>
 
-    <?php
 
-include_once 'includes/footer.php'
+<body>
+  <div class="container-fluid">
+  <div class="row">
+    <div class= "col-xs-12">
+      <?php
 
-?>
+      include_once 'includes/header.php'
+
+      ?>
+    </div>
+  </div>
+
+
+
+  <div class="row">
+    <div class="col-xs-12 middlefront">
+      <div class="centertext">Spelenderwijs Leren<br> Speciaal voor kinderen</div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class= "col-sm-6 topfrontleft">
+      <img id="imagefront" src="img/owl.png"></div>
+    <div class= "col-sm-6 topfront">
+      <div class="toptext"><br><p>Leerzaam, leuk, uitdagend</p>
+        <div class="bottomtext">Getest en mede ontwikkeld door kinderen,<br>
+        voor een optimale leeromgeving.</div></div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 bottomfront">
+
+    </div>
+  </div>
+
+</div>
+
+</body>
+
+</html>
