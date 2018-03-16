b11e59f0f7b1d6540c2858c151af36224b35d852
Marat Petrosov <marat.petrosov@gmail.com>
2018-03-16 17:42:54 +0100

    teachers login

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,8 +1,8 @@
 <?php
     $dbServername = "127.0.0.1";
     $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "spelgoed";
+    $dbPassword = "Hostel@17";
+    $dbName = "elokids";
 
     // Create connection
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
