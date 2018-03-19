3c5ab64084e626508c273f4d3a9cb94d8fb75275
Marat Petrosov <marat.petrosov@gmail.com>
2018-03-19 16:14:21 +0100

    login student done

--- includes/functions.php
+++ includes/functions.php
@@ -6,10 +6,16 @@
     function get_Students(){
         global $conn;
         
-        $sql = "SELECT * FROM students";
+        $sql = "SELECT * FROM `students`  WHERE `st_id` = '".$_SESSION['u_id']."'";
 
-        $students = $conn->query($sql);
-              
+        $result = mysqli_query($conn, $sql); //$conn->query($sql);
+        $students = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+        
+        
+        
         return $students;
              
     }
