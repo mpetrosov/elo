89157c86942afb609744c81e5224b75ffba9cbd9
Marat Petrosov <marat.petrosov@gmail.com>
2018-03-28 10:41:09 +0200

    check

--- game/gamedbmanipulation/connect.php
+++ game/gamedbmanipulation/connect.php
@@ -1,8 +1,15 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "spelgoed";
-     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
- ?>
+   $dbServername = "localhost";
+   $dbUsername = "root";
+   $dbPassword = "Hostel@17";
+   $dbName = "elokids";
+
+    // Create connection
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    // Check connection
+
+
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+
+    }
