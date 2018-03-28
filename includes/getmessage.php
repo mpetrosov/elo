48231284d4236e471ec809106598d8b7c532552c
jjohanj <johanjorritsma@hotmail.com>
2018-03-28 09:25:59 +0200

    start messagesection

--- includes/getmessage.php
+++ includes/getmessage.php
@@ -5,6 +5,6 @@ include('dbh.php');
     $sql = "SELECT * FROM students";
     $result = mysqli_query($conn, $sql);
       foreach ($result as $row) {
-        echo $row['firstname'] . " " . $row['lastname'] . "<br>";}
-
-?>
+        echo "<button onclick=sendMessage()>" . $row['firstname'] . " " . $row['lastname'] . "</button> <br>";
+      }
+        ?>
--- messages.php
+++ messages.php
@@ -1,28 +1,50 @@
 <?php
 include 'includes/dbh.php';
-include 'includes/functions.php';
 include 'includes/header.php';
 ?>
-<?php
-  $students = get_Students();
-?>
 
-<?php foreach ($students as $student):?>
-<div class="col-xs-4"><?=$student['firstname'] . " " . $student['lastname']?></div>
-    <?php endforeach;?>
-
-
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4"><div id="demo"></div>
+          One of three columns
+        </div>
+        <div class="col-sm-4"><div id="demo2"></div>
+          One of three columns
+        </div>
+        <div class="col-sm-4">
+          One of three columns
+        </div>
+      </div>
+    </div>
 <button onclick=getColorSec();>click</button>
+  </body>
+
+
+
+
 
 <script>
-    function getColorSec(link){
+    function getColorSec(){
 
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", "includes/getmessage.php", false);
       xhttp.send();
+
       document.getElementById("demo").innerHTML = xhttp.responseText;
 
       }
+
+
+function sendMessage() {
+      document.getElementById("demo2").innerHTML =
+         '<textarea maxlength="500" cols="80" rows="4"></textarea>' +
+         '<button>send</button>';
+       }
+
+
 </script>
 
-<div id="demo"></div>
+
+
+</html>
