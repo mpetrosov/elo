34ad8f9b2a32c820f7449058ad7ef59087ef9e00
Edwin Zuiderweg <edwinzuiderweg@yahoo.com>
2018-03-28 09:26:59 +0200

    aantal pictures spel flexibel

---
+++ game/gamedbmanipulation/UpdateUserScore.php
@@ -0,0 +1,29 @@
+<?php
+  include 'connect.php';
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $verb = $_SERVER['REQUEST_METHOD'];
+
+
+  if ($verb == "UPDATE") {
+    if (isset($_GET['studentid'])) {
+      $studentid = $_GET['studentid'];
+      $sql = "SELECT Sum(Score) as SomScore FROM MadeLessons WHERE ";
+      $sql .= "studentid = " . $studentid;
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          $SomScore = $row["SomScore"];
+          echo $SomScore;
+          $sql = "UPDATE students SET score = " . $SomScore;
+          $sql .= " WHERE st_id = " . $studentid;
+          $result = $conn->query($sql);
+          die();
+        }
+      }
+    }
+  }
+
+?>
