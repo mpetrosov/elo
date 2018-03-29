2047d4399f1e78467acf72c78d476420a43e5e3f
Marat Petrosov <marat.petrosov@gmail.com>
2018-03-29 13:35:48 +0200

    teachers area

--- includes/functions.php
+++ includes/functions.php
@@ -28,8 +28,31 @@
         $group = $_GET['group'];
 
         //student.php?id=1 
-        $sql = "SELECT lastname, firstname FROM `students`  WHERE `class_id` = $group ORDER BY lastname ASC";
+        $sql = "SELECT st_id, lastname, firstname FROM `students`  WHERE `class_id` = $group ORDER BY lastname ASC";
        
+       
+        $result = mysqli_query($conn, $sql); //$conn->query($sql);
+        
+        while ($row = mysqli_fetch_assoc($result)) {
+            $students[] = $row;
+            
+        }
+        return $students;
+
+             
+    }
+
+    function getStudentsById(){
+        global $conn;
+        if(!isset($_GET['id'])){
+           
+           die();
+        }
+        $id = $_GET['id'];
+
+        //student.php?id=1 
+        $sql = "SELECT * FROM `students`  WHERE `st_id` = $id";
+    //    var_dump($sql);
        
         $result = mysqli_query($conn, $sql); //$conn->query($sql);
         
