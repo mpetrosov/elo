4e37fd3ca45922a8d2278da72b5944a3e4d79788
Marat Petrosov <marat.petrosov@gmail.com>
2018-04-04 15:40:52 +0200

    bonus

--- includes/functions.php
+++ includes/functions.php
@@ -67,20 +67,43 @@
         global $conn;
         $id = $_GET['id'];
         
-        $sql = "SELECT bonus FROM `students`  WHERE `st_id` = $id";
+        $sql = "UPDATE students SET bonus = bonus + 1 WHERE st_id = $id";
    
        
         $result = mysqli_query($conn, $sql); 
-        
+       
+        $sql = "SELECT bonus from students WHERE st_id = $id";
+        $result = mysqli_query($conn, $sql);
+        $rows = [];
+
         while ($row = mysqli_fetch_assoc($result)) {
-            $students[] = $row;
-            
+            $rows[] = $row;
         }
 
-        // for(i = 0; i < 10 i++){
+        return $rows;
+        // $records = [];
 
+        // while ($row = mysqli_fetch_assoc($result)) {
+        //     $records[] = $row;
         // }
-        return $students;
+
+        // if (count($records) > 0) {
+        //     $bonus = $records[0]['bonus'];
+        //     // $bonus = $records[0];
+        //     var_dump($bonus++);
+        //     die();
+        //     $bonus ++;
+        //     $sql = "UPDATE `students` SET `bonus` = $bonus";
+
+        //     mysqli_query($conn, $sql) or die(mysqli_error($conn));
+        // }
+ 
+        // for (i = 0; $row['i'] < 10; i++){
+        //     $students = $row['i'];
+           
+        // }var_dump($bonusses);
+        
+    
        
 
 
--- userpage.php
+++ userpage.php
@@ -2,6 +2,7 @@
 include 'includes/dbh.php';
 include 'includes/functions.php';
 include 'includes/header.php';
+  
 ?>
   <body  Onload = "DetermineFinishedLessons()">
     <div class="container">
@@ -12,9 +13,10 @@ include 'includes/header.php';
     <div class="row">
       <div class="col-sm-8 usertop">
         <div class="row">
-        <div class="col-xs-8 welcome">Hallo <?=$student['firstname']?>
+          <div class="col-xs-8 welcome">Hallo <?=$student['firstname']?></div>
+          
 
-        </div>
+        
         <div class="col-xs-4"><img class="avatar" src=<?=$student['avatar']?>></div>
         </div>
       </div>
@@ -87,9 +89,28 @@ include 'includes/header.php';
         <center><div id = "divLessonFinished4" class = "LessonFinished">V
         </div></center>
       </div>
+      
     </div>
-    </div>
+    <div class="row">
+        <div class="col-sm-12 bonus">
+          <?php
+          $id = $_SESSION['u_id'];
 
+
+          $sql = "SELECT bonus FROM students  WHERE st_id = $id";
+          $result = mysqli_query($conn, $sql);
+          $row = mysqli_fetch_assoc($result);
+
+          $bonus = $row['bonus'];
+
+          for($i = 0; $i<$bonus; $i++){
+          echo"<img class='star' src='img/star.png'>";
+          }
+        ?>
+        </div>
+    
+    </div>
+  </div>
   <br><br>
 
   </body>
