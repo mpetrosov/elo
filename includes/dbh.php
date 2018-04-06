97043f3ffaad632ffe1d35e907089f36f3699356
Edwin Zuiderweg <edwinzuiderweg@yahoo.com>
2018-04-06 10:28:04 +0200

    vogeltjes ophalen

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,14 +1,8 @@
 <?php
    $dbServername = "localhost";
-<<<<<<< HEAD
-   $dbUsername = "root";
-   $dbPassword = "Hostel@17";
-   $dbName = "elokids";
-=======
    $dbUsername = "Edmaster";
    $dbPassword = "welkom";
    $dbName = "Spelgoed";
->>>>>>> meerkeuze toegevoegd
 
     // Create connection
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
